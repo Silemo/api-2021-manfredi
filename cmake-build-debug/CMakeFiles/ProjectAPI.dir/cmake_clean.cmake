07a0887a3e0efc8a48b4e2f7edc636f01ba7b9f8
Silemo <59120330+Silemo@users.noreply.github.com>
2021-07-22 12:01:02 +0200

    Initial Setup

---
+++ cmake-build-debug/CMakeFiles/ProjectAPI.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProjectAPI.dir/main.c.o"
+  "ProjectAPI"
+  "ProjectAPI.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ProjectAPI.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
